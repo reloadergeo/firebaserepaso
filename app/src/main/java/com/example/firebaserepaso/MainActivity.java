f91edb380cf73483e8c792c088454daf805c75ce
Resembrink <reloader1982@gmail.com>
2018-09-03 13:59:21 -0300

    13:58 iniciando actualizaciones

--- app/src/main/java/com/example/firebaserepaso/MainActivity.java
+++ app/src/main/java/com/example/firebaserepaso/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.firebaserepaso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edt_usuario, edt_contrasenia;
+    Button btn_registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edt_contrasenia= findViewById(R.id.edt_contrasenia);
+        edt_usuario=findViewById(R.id.edt_usuario_correo);
+        btn_registrar=findViewById(R.id.btn_registrar);
+
     }
 }
